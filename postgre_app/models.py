2c2df0c39a0543c12ef99ecb749a0fed700fd350
empty-works <pama.mark@gmail.com>
2024-02-23 06:25:49 +0100

    Added patient table to the postgres models file.

--- postgre_app/models.py
+++ postgre_app/models.py
@@ -17,3 +17,34 @@ class User(Base):
     facility_id = Column(String, nullable=False)
     enabled = Column(Boolean, nullable=False)
     hashed_password = Column(String, nullable=False)
+
+    patient = relationship("Patient", back_populates="user")
+
+class Patient(Base):
+    __tablename__ = "patients"
+
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    patient_provider = Column(String, nullable=False)
+    patient_provider_id = Column(String, nullable=False)
+    patient_room = Column(String)
+    patient_current_gender = Column(String, nullable=False)
+    patient_type = Column(String, nullable=False)
+    patient_language_preference = Column(String)
+    patient_street_address = Column(String)
+    patient_city = Column(String)
+    patient_state = Column(String)
+    patient_country = Column(String)
+    patient_phone_number = Column(String)
+    patient_gender_at_birth = Column(String, nullable=False)
+    patient_sexual_orientation = Column(String, nullable=False)
+    patient_marital_status = Column(String, nullable=False)
+    patient_living_arrangement = Column(String, nullable=False)
+    patient_is_adopted = Column(Boolean, nullable=False)
+    patient_license_number = Column(String)
+    patient_vehicle_serial_number = Column(String)
+    patient_vehicle_plate_number = Column(String)
+    patient_url = Column(String)
+    patient_device_serial_number = Column(String)
+    patient_ip_address = Column(String)
+
+    user = relationship("User", back_populates="patient")
