4ef87d19a221abd0e1f1457469e310ed74156d35
empty-works <pama.mark@gmail.com>
2024-04-05 17:02:33 +0200

    Path operation functions added in main to handle User and UserActivityLog objects.

--- main.py
+++ main.py
@@ -23,6 +23,14 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app = FastAPI()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def verify_password(plain_password, hashed_password):
@@ -31,6 +39,7 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
+'''
 hash_pwd1 = get_password_hash("fakehashedsecret1")
 hash_pwd2 = get_password_hash("fakehashedsecret2")
 
@@ -60,6 +69,7 @@ fake_users_db = {
         "enabled": True
     },
 }
+'''
 
 class Token(BaseModel):
     access_token: str
@@ -119,7 +129,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = get_user(get_db, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
@@ -134,7 +144,7 @@ async def get_current_active_user(
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
     ) -> Token:
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    user = authenticate_user(get_db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -150,3 +160,33 @@ async def login_for_access_token(
 @app.get("/users/me")
 async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
     return current_user
+
+@app.post("/users/", response_model=schemas.User)
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_username(db, username=user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already exists")
+    return crud.create_user(db=db, user=user)
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@app.post("/users/{user_id}/user_activity_logs", response_model=schemas.UserActivityLog)
+def create_activity_log_for_user(
+    user_id: int, user_activity_log: schemas.UserActivityLogCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_activity_log(db=db, user_activity_log=user_activity_log, user_id=user_id)
+
+@app.get("/user_activity_logs/", response_model=list[schemas.UserActivityLog])
+def read_user_activity_logs(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    user_activity_logs = crud.get_user_activity_logs(db, skip=skip, limit=limit)
+    return user_activity_logs
